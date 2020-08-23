553d3b1054563a209b41eff60d5d561f7e142556
pouya pormohamad <p.pormohamad@gmail.com>
2020-08-23 19:54:50 +0430

    set version

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use PHPHtmlParser\Crawl\WebPageProcessor;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$data = WebPageProcessor::onePageProcessed('https://www.bambin.ir');
-dump($data);
